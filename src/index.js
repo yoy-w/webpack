b089ad341ce91791e5c20dc910ec0430c70cd8b7
yaomianjie <754083746@qq.com>
2021-08-16 22:46:50 +0800

    对于sideEffects参数配置

--- src/index.js
+++ src/index.js
@@ -1,16 +1,1 @@
-// // import _ from 'loadsh'
-
-// // console.log(_.join(['A','B','C']))
-
-// let total = 0
-// let count = 1
-// function add() { 
-//   if(count >= 101){
-//     return total
-//   }
-//   total = total+count
-//   count++
-//   add()
-// }
-// add()
-// console.log(total)
+import './style.css'
